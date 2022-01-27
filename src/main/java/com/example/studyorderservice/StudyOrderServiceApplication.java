48046be43099437af3fc6afebe7c3310cdfe8650
shk3029 <shk30290@naver.com>
2022-01-27 23:40:40 +0900

    Initial commit

---
+++ src/main/java/com/example/studyorderservice/StudyOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyorderservice/StudyOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
