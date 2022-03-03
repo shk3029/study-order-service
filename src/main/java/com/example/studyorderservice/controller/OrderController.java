103f0038deb22bb4388f901c080eac8d38b6c14d
shk3029 <shk30290@naver.com>
2022-03-03 21:32:50 +0900

    feat: spring cloud sleuth + Zipkin 이용한 분산추적

--- src/main/java/com/example/studyorderservice/controller/OrderController.java
+++ src/main/java/com/example/studyorderservice/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.example.studyorderservice.service.OrderService;
 import com.example.studyorderservice.vo.RequestOrder;
 import com.example.studyorderservice.vo.ResponseOrder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/order-service")
 @RequiredArgsConstructor
@@ -33,6 +35,7 @@ public class OrderController {
     //
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable String userId, @RequestBody RequestOrder orderDetail) {
+        log.info("Before add orders data");
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
@@ -41,18 +44,22 @@ public class OrderController {
         OrderDto createdOrder = orderService.createOrder(orderDto);
 
         ResponseOrder responseUser = mapper.map(createdOrder , ResponseOrder.class);
-
+        log.info("After add orders data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
 
     @GetMapping("/{userId}/orders")
     public ResponseEntity<List<ResponseOrder>> getOrders(@PathVariable("userId") String userId) {
+        log.info("Before retrieve orders data");
+
         Iterable<OrderEntity> orders = orderService.getOrdersByUserId(userId);
         List<ResponseOrder> result = new ArrayList<>();
 
         orders.forEach(orderEntity -> {
             result.add(new ModelMapper().map(orderEntity, ResponseOrder.class));
         });
+
+        log.info("Add retrieve orders data");
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
